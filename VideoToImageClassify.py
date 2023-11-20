5d06dd89b207494de12130eff752840b176eb32b
QueenKimm <gudarzikimia@gmail.com>
2023-11-20 16:25:42 -0500

    Generating images from the videos and classifying sleepy/ not sleepy images

--- VideoToImageClassify.py
+++ VideoToImageClassify.py
@@ -7,9 +7,10 @@ import os
 import cv2
 import zipfile
 import requests
+import random
+#
 # from tensorflow import layers, models
 from sklearn.model_selection import train_test_split
-
 
 file_url = "https://orbi.uliege.be/bitstream/2268/191620/4/DROZY.zip"
 local_file_dir = "Videos"
@@ -100,3 +101,47 @@ for label in range(10):
                 print(f"Moving {src} to {dst}")
     else:
         print(f"Directory {label_dir} does not exist")
+
+organized_directory = "Videos/organized_images"
+
+# Prefixes for the 'not sleepy' class (0_to_4) and 'sleepy' class (5_to_9)
+not_sleepy_prefixes = ['1-1', '2-1', '3-1', '4-1', '5-1', '6-1', '7-2', '8-1', '10-1', '11-1', '12-1', '13-2']
+sleepy_prefixes = ['1-2', '2-2', '4-2', '5-2', '6-3', '7-3', '8-2', '9-2', '10-3', '11-2', '13-1', '14-1']
+
+# Create the test_dataset directory
+test_dataset_dir = os.path.join(organized_directory, "test_dataset")
+os.makedirs(test_dataset_dir, exist_ok=True)
+
+
+# Function to move random image with specified prefixes to test_dataset
+def move_random_image(prefixes, source_dir, destination_dir):
+    selected_files = []
+    for prefix in prefixes:
+        # List all files starting with the prefix
+        matching_files = [f for f in os.listdir(source_dir) if f.startswith(prefix)]
+        # If there are matching files, randomly select one and move it
+        if matching_files:
+            selected_file = random.choice(matching_files)
+            src_path = os.path.join(source_dir, selected_file)
+            dst_path = os.path.join(destination_dir, selected_file)
+            shutil.move(src_path, dst_path)
+            selected_files.append(selected_file)
+            print(f"Moved {selected_file} to {destination_dir}")
+    return selected_files
+
+
+# Move images from 0_to_4 to test_dataset
+move_random_image(not_sleepy_prefixes, binary_class_directories['0_to_4'], test_dataset_dir)
+
+# Move images from 5_to_9 to test_dataset
+move_random_image(sleepy_prefixes, binary_class_directories['5_to_9'], test_dataset_dir)
+
+# from PIL import Image
+# import os
+#
+# # Replace 'path_to_one_image.jpg' with the actual path to one of your images
+# image_path = 'Videos/organized_images/0_to_4/1-1.mp4_frame7362.jpg'
+#
+# # Open the image and print its size
+# with Image.open(image_path) as img:
+#     print(img.size)  # Outputs: (width, height)
